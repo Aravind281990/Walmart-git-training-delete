076d8b6caaf304c179ff4043fe43dec01bee1f78
Aravinthraj <aravindraj303@gmail.com>
2024-09-22 16:18:27 +0530

    Created sub function

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,10 +5,15 @@ public class HelloWorld {
              {
                System.out.println("Hello");
                add(10,20);
+               sub(20,10);
              }
 
         public static int add(int num1, int num2) {
               return num1 + num2;
+        }
+
+        public static int sub(int num1, int num2) {
+              return num1 - num2;
         }
 
 } 
