1a82efc26c5774c24b7f0d6a95b79ad6c392ce4a
Aravinthraj <aravindraj303@gmail.com>
2024-09-22 15:43:52 +0530

    Created HelloWorld.java file for testing

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package com.aravind;
+
+public class HelloWorld{
+
+        public static void main (String [] args)
+             {
+               System.out.println("Hello");
+             }
+
+} 
+
