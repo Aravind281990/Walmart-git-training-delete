51c98a37c9f67357b49dc9dc1e596a49f551f00a
Aravinthraj <aravindraj303@gmail.com>
2024-09-22 16:15:55 +0530

    Invoking add function

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
         public static void main (String [] args)
              {
                System.out.println("Hello");
+               add(10,20);
              }
 
         public static int add(int num1, int num2) {
